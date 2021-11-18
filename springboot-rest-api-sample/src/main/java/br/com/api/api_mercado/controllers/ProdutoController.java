e7ea5abbbff3e898396cbd40dedffc3526d2d9f7
Vinicius Santana <viniciussantanaleao@gmail.com>
2021-11-18 20:40:49 -0300

    -Refatoração

--- springboot-rest-api-sample/src/main/java/br/com/api/api_mercado/controllers/ProdutoController.java
+++ springboot-rest-api-sample/src/main/java/br/com/api/api_mercado/controllers/ProdutoController.java
@@ -7,55 +7,51 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.api.api_mercado.model.Funcionario;
 import br.com.api.api_mercado.model.Login;
+import br.com.api.api_mercado.model.Produto;
 import br.com.api.api_mercado.repository.LoginRepository;
+import br.com.api.api_mercado.repository.ProdutoRepository;
 
 
 @RestController
 public class ProdutoController {
 	
 	@Autowired /*  CDI - INJEÇÃO DE DEPENDENCIA   */
-	private LoginRepository loginRepository;
+	private ProdutoRepository produtoRepository;
 	
 	@PostMapping(value = "cadastrarProduto")
     @ResponseBody
-    public ResponseEntity<?> cadastrarLogin(@RequestBody @Valid Login usr){
+    public ResponseEntity<?> cadastrarLogin(@RequestBody @Valid Produto prod){
 		
-		usr.setUserNome(usr.getUserNome().trim());
+		prod.setNome(prod.getNome().trim());
 	
-		List<Login> listaLogin = loginRepository.findAll();
+		List<Produto> listaPro = produtoRepository.findAll();
 		
-		for (Login login : listaLogin) {
-			if(login.getUserNome().contains(usr.getUserNome())) {
-				return new ResponseEntity<String>("Este Usuario ja existe !!", HttpStatus.OK);
+		for (Produto pro : listaPro) {
+			if(pro.getNome().contains(prod.getNome())) {
+				return new ResponseEntity<String>("Este Produto ja existe !!", HttpStatus.OK);
 			}
 		}
-		Login user = loginRepository.save(usr);
+		Produto pro = produtoRepository.save(prod);
 			
-		return new ResponseEntity<Login>(user, HttpStatus.CREATED);
+		return new ResponseEntity<Produto>(pro, HttpStatus.CREATED);
     }
 	
-	@PostMapping(value = "login")
-    @ResponseBody
-    public ResponseEntity<?> efetuarLogin(@RequestBody @Valid Login usr){
-		
-		usr.setUserNome(usr.getUserNome().trim());
-		
-		List<Login> listaLogin = loginRepository.findAll();
-		
-		for (Login login : listaLogin) {
-			if(login.getUserNome().contains(usr.getUserNome())) {
-				return new ResponseEntity<Login>(usr, HttpStatus.OK);
-			}
-		}	
-		
-		return new ResponseEntity<String>("Este Login de User NAO EXISTE !!", HttpStatus.OK);
-		
+	@GetMapping(value = "listarProduto")
+    @ResponseBody /*  Retorna os dados paara o corpo da resposta   */
+    public ResponseEntity<List<Produto>> listaProduto(){
+    	
+    	/*  Chama a interface que usa o metodo extendido da Interface JPArEPOSITORY  */
+    	List<Produto> produtos = produtoRepository.findAll();
+    	
+    	return new ResponseEntity<List<Produto>>(produtos, HttpStatus.OK); /*  Retorna a lista em json  */
     }
 
 }
---
+++ springboot-rest-api-sample/src/main/java/br/com/api/api_mercado/repository/ProdutoRepository.java
@@ -0,0 +1,17 @@
+package br.com.api.api_mercado.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import org.springframework.stereotype.Repository;
+
+import br.com.api.api_mercado.model.Login;
+import br.com.api.api_mercado.model.Produto;
+
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Long>{
+	
+	//@Query(value = "select lo from Login lo where lo.user = :name ")
+	//List<Login> buscaLogin(String name);
+
+}
